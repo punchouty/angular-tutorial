0373c884b2f20df537e91cca3fd992797d451de9
Rajan Punchouty <rajan@datiot.com>
2016-07-08 13:32:47 +0530

    Integration with Karma complete

---
+++ README.md
@@ -0,0 +1,4 @@
+npm install karma --save-dev
+sudo npm install -g karma-cli
+npm install karma-jasmine karma-chrome-launcher --save-dev
+npm install karma-phantomjs-launcher --save-dev
--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,33 @@
 module.exports = function(config) {
   config.set({
 
-    
+    //basePath: './app',
+
+    files: [
+      'lib/angular.js',
+      'lib/angular-resource.js',
+      'lib/angular-ui-router.js',
+      'lib/angular-mock.js',
+      'js/**/*.js',
+      'unit/**/*.js',
+      'e2e-tests/**/*.js'
+    ],
+
+    logLevel: config.LOG_INFO, // LOG_DEBUG
+
+    autoWatch: true,
+
+    frameworks: ['jasmine'],
+
+    browsers: ['PhantoJS'], // ['Chrome','PhantoJS']
+
+    // plugins: [
+    //   'karma-chrome-launcher',
+    //   'karma-firefox-launcher',
+    //   'karma-phantomjs-launcher',
+    //   'karma-jasmine',
+    //   'karma-junit-reporter'
+    // ],
 
   });
 };
